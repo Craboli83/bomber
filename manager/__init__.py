f2fd3dd9c190c68719349cf66d414634dc744228
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-12 21:29:29 +0330

    Update __init__.py

--- manager/__init__.py
+++ manager/__init__.py
@@ -1,21 +1,20 @@
 from telethon import TelegramClient
+from telethon.sessions import StringSession
 import sys
 
 API_ID = 26909317
 API_HASH = "bae2fc6f671b02f9bea0473ed369a95f"
-BOT_TOKEN = "6069204518:AAEDwif5aO8hw6f8-axXY1R2Dd9H5va5RwY"
+BOT_SESSION = "1BJWap1wBuyC5s2IhBjW3T-mSwB48QThF8MqJj8DC5VHPu0QlSS4LCP7WXOEAswUhE6gpyFDVE0SQQcbu7QsyFx0Bt_xcRUig1uqXzp4DNoqSWfbR5hgD0dqwkHgKHwF7XFWEGLaQ0A9sTYuK5UXaPZomVxOrIbSiu5NIY0e06xQGcDLxvN3aCL-WWD96ecIrWwrDGAJsMOm12H5TYpKQFAQ1K1Er9VehtvroI_ohjMId54Elwr74niTo_9nzYMvXgf5lZ-CL1_ZT8H2dBk0fFz87KarPGGQBNvTBu9IiTdgpR34onl2sB2RErCq1uPKIQvn6WZAyBjryFN8RCWf2uBB5o9L412U="
 LOG_GROUP = "GroupFidoTemp"
 CHANNEL = "BackUpFidoTemp"
 ADMIN_ID = 5889107490
 
-print("• Starting Bot . . .")
-
 try:
     bot = TelegramClient(
-        session="AccManagerBot",
+        session=StringSession(BOT_SESSION),
         api_id=API_ID,
         api_hash=API_HASH,
-    ).start(bot_token=BOT_TOKEN)
+    ).start()
 except Exception as e:
-    print(f"• Error On Create Bot: {e}")
+    print(f"• Error: {e}")
     sys.exit()
