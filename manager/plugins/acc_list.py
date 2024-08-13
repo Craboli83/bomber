397a70c8834f309a2053b5405bc48458397b053b
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-13 21:11:32 +0330

    Update acc_list.py

--- manager/plugins/acc_list.py
+++ manager/plugins/acc_list.py
@@ -21,7 +21,7 @@ async def myaccs(event):
             status = "✅" if client else "❌"
             text += f"**{count} - {flag}** `{acc}` ( `{status}` )\n"
             count += 1
-        buttons = [[Button.inline("❌ No ❌", data=f"getaccs:{event.sender_id}")]]
+        buttons = [[Button.inline("• Get Sessions •", data=f"getaccs:{event.sender_id}")]]
         await event.reply(text, buttons=buttons)
     else:
         text = f"📋 Your Accounts List:\n💡 Count: ( {len(accs)} )\n\n"
@@ -35,7 +35,7 @@ async def myaccs(event):
             count += 1
         open(f"{event.sender_id}.txt", "w").write(str(text))
         text = f"**📋 Your Accounts List:**\n\n**💡 Count:** ( `{len(accs)}` )"
-        buttons = [[Button.inline("❌ No ❌", data=f"getaccs:{event.sender_id}")]]
+        buttons = [[Button.inline("• Get Sessions •", data=f"getaccs:{event.sender_id}")]]
         await event.reply(text, file=f"{event.sender_id}.txt", buttons=buttons)
 
 @Callback(data="getaccs\:(.*)")
@@ -45,9 +45,9 @@ async def yesedit(event):
     text = f"💡 Count: ( {len(accs)} )\n\n"
     for acc in accs:
         session = accs[acc]
-        text += f"{acc} - {status}\n"
+        text += f"{acc} - {session}\n"
     fname = str(userid) + ".txt"
     open(fname, "w").write(text)
-    text = text = f"**📋 Your Accounts Sessions List!**\n\n**💡 Count:** ( `{len(accs)}` )"
+    text = f"**📋 Your Accounts Sessions List!**\n\n**💡 Count:** ( `{len(accs)}` )"
     await event.reply(text, file=fname)
     os.remove(fname)
