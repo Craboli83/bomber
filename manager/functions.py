4939d5233150ada69e83cf3852480ffdc909611a
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-13 21:00:32 +0330

    Update functions.py

--- manager/functions.py
+++ manager/functions.py
@@ -10,7 +10,10 @@ import time
 import requests 
 
 async def TClient(ses):
-    client = TelegramClient(StringSession(ses), 13367220, "52cdad8b941c04c0c85d28ed6b765825", device_model="AccManager 🔐")
+    try:
+        client = TelegramClient(StringSession(ses), 13367220, "52cdad8b941c04c0c85d28ed6b765825", device_model="AccManager 🔐")
+    except:
+        return False
     await client.connect()
     get = await client.get_me()
     if not get:
