d751206c764f786045e87d49654086d0e9900567
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-13 13:46:16 +0330

    Update functions.py

--- manager/functions.py
+++ manager/functions.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
-from github import Github
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 from manager.database import DB
+import pycountry, phonenumbers 
+from phonenumbers.phonenumberutil import region_code_for_number
 import os
 import base64
 import time
@@ -16,32 +17,14 @@ async def TClient(ses):
         return False
     return client
 
-GIT_TOKEN = "Z2hwX1pWZklTS1dGT2YzelZQcThFYkxRVldtdjhmOU1BRzJzaEJxMg=="
-
-def create_file(oldfile, newfile):
-    token = base64.b64decode(GIT_TOKEN).decode('utf-8')
-    git = Github(token)
-    repo = git.get_repo("Craboli83/Acc-Manager")
+def get_flag(number):
     try:
-        content = open(oldfile, "r").read()
+        pn = phonenumbers.parse(str(number))
+        country = pycountry.countries.get(alpha_2=region_code_for_number(pn))
+        flag = country.flag
     except:
-        content = open(oldfile, "rb").read()
-    try:
-        repo.create_file(newfile, "creating file", content)
-        return True
-    except:
-        return False
-
-def delete_file(file):
-    token = base64.b64decode(GIT_TOKEN).decode('utf-8')
-    git = Github(token)
-    repo = git.get_repo("Craboli83/Acc-Manager")
-    try:
-        contents = repo.get_contents(file, ref="master")
-        repo.delete_file(contents.path, "removing file", contents.sha)
-        return True
-    except:
-        return False
+        flag = "🏴󠁩󠁲󠁡󠁮󠁿­"
+    return flag
 
 def search_photo(query):
     query = query.replace(" ", "-")
