88b36556aa458232791bd3478bc2d658aa66a29f
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-14 14:07:48 +0330

    update manager/plugins/__init__.py

--- manager/plugins/__init__.py
+++ manager/plugins/__init__.py
@@ -34,6 +34,7 @@ def panel_menu():
         [Button.inline(f"{status} Bot Status {status}", data="onoff")],
         [Button.inline("📤 Send To All 📤", data="sendtoall"), Button.inline("📤 Send To User 📤", data="sendtouser")],
         [Button.inline("📝 Get Users 📝", data="getusers")],
+        [Button.inline("✅ Add Vip 👑", data="addvip"), Button.inline("❌ Del Vip 👑", data="delvip")],
     ]
     return menu
 
