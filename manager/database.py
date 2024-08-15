8c93a940c7fedf40a56dca0a09b705e9674a4bd3
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-15 10:10:51 +0330

    update manager/database.py

--- manager/database.py
+++ manager/database.py
@@ -1,76 +1,46 @@
-from redis import Redis
-from localdb import Database
 import json
 
-def get_data(self, key):
-    data = self.get(str(key))
-    if data:
-        try:
-            data = eval(data)
-        except BaseException:
-            pass
-    return data
-
-class RedisDB:
-    def __init__(self):
-        MainUrl = "redis-10247.c59.eu-west-1-2.ec2.redns.redis-cloud.com:10247"
-        URL = (MainUrl).split(":")[0]
-        PORT = (MainUrl).split(":")[-1]
-        self.db = Redis(
-                host=URL,
-                password="MgX2INYxyA8S5lCvZCtuITmmDkgmpPbi",
-                port=int(PORT),
-                decode_responses=True,
-            )
-        self.set = self.db.set
-        self.get = self.db.get
-        self.keys = self.db.keys
-        self.ping = self.db.ping
-        self.delete = self.db.delete
-        self.recache()
-
-    def recache(self):
+class Database:
+    def __init__(self, database_name):
         self.cache = {}
-        for keys in self.keys():
-            self.cache.update({keys: self.get_key(keys)})
+        self.dbname = database_name
+        self.re_data()
 
-    @property
-    def name(self):
-        return "Redis"
+    def get_data(self):
+        if os.path.isfile(self.dbname):
+            fdata = open(self.dbname, "r")
+            data = fdata.read()
+        else:
+            ndata = open(self.dbname, "w")
+            ndata.write(str({}))
+            data = str({})
+        return data
 
-    @property
-    def usage(self):
-        allusage = 0
-        for x in self.keys():
-            usage = self.db.memory_usage(x)
-            allusage += usage
-        return allusage
+    def re_data(self):
+        data = self.get_data()
+        self.cache = eval(self.get_data())
 
-    def set_key(self, key, value):
-        value = str(value)
-        try:
-            value = eval(value)
-        except BaseException:
-            pass
-        self.cache.update({key: value})
-        return self.set(str(key), str(value))
-
-    def get_key(self, key):
+    def get(self, key):
         if key in self.cache:
-            return self.cache[key]
-        get = get_data(self, key)
-        self.cache.update({key: get})
-        return get
+            return self.cache.get(key)
 
-    def del_key(self, key):
-        if key in self.cache:
-            del self.cache[key]
-        return bool(self.delete(str(key)))
-        
-    def flushall(self):
-        for x in self.keys():
-            self.del_key(x)
+    def set(self, key=None, value=None, delete_key=None):
+        data = self.cache
+        if delete_key:
+            try:
+                del data[delete_key]
+            except KeyError:
+                pass
+        if key and value:
+            data.update({key: value})
+        with open(self.dbname, "w") as dbfile:
+            json.dump(data, dbfile)
+        self.re_data()
         return True
+
+    def delete(self, key):
+        if key in self.cache:
+            return self.set(delete_key=key)
 
 class LocalDB:
     def __init__(self):
@@ -81,33 +51,15 @@ class LocalDB:
         self.cache = {}
         self.recache()
 
-    def get_key(self, key):
-        if key in self.cache:
-            value = self.cache[key]
-        else:
-            value = self._get_data(key)
-            self.cache.update({key: value})
-        try:
-            data = eval(value)
-        except:
-            data = value
-        return data
-
+    def keys(self):
+        return []
+        
     def recache(self):
         self.cache.clear()
         for key in self.keys():
             self.cache.update({key: self.get_key(key)})
 
-    def keys(self):
-        return []
-
-    def del_key(self, key):
-        if key in self.cache:
-            del self.cache[key]
-        self.delete(key)
-        return True
-
-    def _get_data(self, key=None, data=None):
+    def get_data(self, key=None, data=None):
         if key:
             data = self.get(str(key))
         if data and isinstance(data, str):
@@ -117,11 +69,27 @@ class LocalDB:
                 pass
         return data
 
-    def set_key(self, key, value, cache_only=False):
-        value = self._get_data(data=value)
+    def set_key(self, key, value):
+        value = self.get_data(data=value)
         self.cache[key] = value
-        if cache_only:
-            return
         return self.set(str(key), str(value))
+
+    def del_key(self, key):
+        if key in self.cache:
+            del self.cache[key]
+        self.delete(key)
+        return True
+
+    def get_key(self, key):
+        if key in self.cache:
+            value = self.cache[key]
+        else:
+            value = self.get_data(key)
+            self.cache.update({key: value})
+        try:
+            data = eval(value)
+        except:
+            data = value
+        return data
 
 DB = LocalDB()
