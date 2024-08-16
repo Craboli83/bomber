7d53baa477e40517d150b997f5b0ea7910ac2d74
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-16 17:22:53 +0330

    update manager/plugins/__init__.py

--- manager/plugins/__init__.py
+++ manager/plugins/__init__.py
@@ -4,8 +4,8 @@ from manager.database import DB
 
 def main_menu(event):
     menu = [
-        [Button.text("Add Account 📥", resize=True)],
-        [Button.text("Add Session 🔗", resize=True)],
+        [Button.text("Add Account 📥", resize=True), Button.text("Add Session 🔗", resize=True)],
+        [Button.text("Fake Email 📨", resize=True)],
         [Button.text("Account Settings ⚙️", resize=True), Button.text("Accounts List 📋", resize=True)],
         [Button.text("Account Panel 🛠️", resize=True), Button.text("My Info 📝", resize=True)],
         [Button.text("Guide 💡", resize=True), Button.text("Support 🧒", resize=True)],
