3044cbd7d476cad6f8a576ae0f80de66a0867529
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-19 01:43:33 +0330

    Update database.py

--- manager/database.py
+++ manager/database.py
@@ -1,6 +1,77 @@
+from redis import Redis
 from manager import DB_NAME
 import json
 import os
+
+def get_data(self, key):
+    data = self.get(str(key))
+    if data:
+        try:
+            data = eval(data)
+        except BaseException:
+            pass
+    return data
+
+class RedisDB:
+    def __init__(self):
+        MainUrl = "redis-10247.c59.eu-west-1-2.ec2.redns.redis-cloud.com:10247"
+        URL = (MainUrl).split(":")[0]
+        PORT = (MainUrl).split(":")[-1]
+        self.db = Redis(
+                host=URL,
+                password="MgX2INYxyA8S5lCvZCtuITmmDkgmpPbi",
+                port=int(PORT),
+                decode_responses=True,
+            )
+        self.set = self.db.set
+        self.get = self.db.get
+        self.keys = self.db.keys
+        self.ping = self.db.ping
+        self.delete = self.db.delete
+        self.recache()
+
+    def recache(self):
+        self.cache = {}
+        for keys in self.keys():
+            self.cache.update({keys: self.get_key(keys)})
+
+    @property
+    def name(self):
+        return "Redis"
+
+    @property
+    def usage(self):
+        allusage = 0
+        for x in self.keys():
+            usage = self.db.memory_usage(x)
+            allusage += usage
+        return allusage
+
+    def set_key(self, key, value):
+        value = str(value)
+        try:
+            value = eval(value)
+        except BaseException:
+            pass
+        self.cache.update({key: value})
+        return self.set(str(key), str(value))
+
+    def get_key(self, key):
+        if key in self.cache:
+            return self.cache[key]
+        get = get_data(self, key)
+        self.cache.update({key: get})
+        return get
+
+    def del_key(self, key):
+        if key in self.cache:
+            del self.cache[key]
+        return bool(self.delete(str(key)))
+        
+    def flushall(self):
+        for x in self.keys():
+            self.del_key(x)
+        return True
 
 class Database:
     def __init__(self):
@@ -94,4 +165,4 @@ class LocalDB:
             data = value
         return data
 
-DB = LocalDB()
+DB = RedisDB()
