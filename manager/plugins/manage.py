932a1e1fe9a8fb65c0ab4c90567728033b9cac59
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-29 10:26:57 +0330

    update manager/plugins/manage.py

--- manager/plugins/manage.py
+++ manager/plugins/manage.py
@@ -59,15 +59,39 @@ async def editacc(event):
             pass
     await event.edit(f"**✅ Your Account Successfuly Edited!**\n\n**📱 Phone:** ( {flag} `{phone}` {flag} )")
 
-@Callback(data="delacc:(.*)")
-async def logout(event):
+@Callback(data="infoacc:(.*)")
+async def infoacc(event):
     phone = str(event.pattern_match.group(1).decode('utf-8'))
     flag = get_flag(phone)
-    allaccs = DB.get_key("USER_ACCS")[event.sender_id]
-    if phone in allaccs:
-        all = DB.get_key("USER_ACCS_COUNT")
-        all[event.sender_id] -= 1
-        DB.set_key("USER_ACCS_COUNT", all)
+    info = None
+    try:
+        info = await bot.client.get_entity(phone)
+    except:
+        contact = await bot.client(functions.contacts.ImportContactsRequest(contacts=[types.InputPhoneContact(client_id=random.randrange(-2**63, 2**63), phone=phone, first_name=phone, last_name="")]))
+        if contact.users:
+            info = contact.users[0]
+    if info:
+        username = f"@{info.username}" if info.username else "---"
+        cont = "✅" if info.contact else "❌"
+        strst = "%e %B %Y | %H:%M"
+        if info.status:
+            ustats = info.status.to_dict()["_"].replace("UserStatus", "")
+            if ustats == "Offline":
+                utime = info.status.was_online.strftime(strst)
+                stats = ustats + " - " + utime
+            else:
+                stats = ustats
+        else:
+            stats = "---"
+        userinfo = f"""**💠 Number Info:** ( {flag} `{phone}` {flag} )\n\n**• ID:** ( `{info.id}` )\n**• First Name:** ( `{info.first_name}` )\n**• Last Name:** ( `{info.last_name or "---"}` )\n**• Username :** ( `{username}` )\n**• Contact:** ( `{cont}` )\n\n**• Status:** ( `{stats}` )"""
+        await event.reply(userinfo)
+    else:
+        await event.reply(f"**❌ Can't Get Information For Your Number:** ( {flag} `{phone}` {flag} )")
+
+@Callback(data="delacc:(.*)")
+async def delacc(event):
+    phone = str(event.pattern_match.group(1).decode('utf-8'))
+    flag = get_flag(phone)
     allaccs = DB.get_key("USER_ACCS")
     del allaccs[event.sender_id][phone]
     DB.set_key("USER_ACCS", allaccs)
@@ -85,11 +109,6 @@ async def logout(event):
         return await event.edit(f"**❗ This Account Is Out Of Reach Of The Robot!**\n\n__❔ Do You Want To Delete It From The List Of Accounts?__", buttons=buttons)
     await client.connect()
     await client.log_out()
-    allaccs = DB.get_key("USER_ACCS")[event.sender_id]
-    if phone in allaccs:
-        all = DB.get_key("USER_ACCS_COUNT")
-        all[event.sender_id] -= 1
-        DB.set_key("USER_ACCS_COUNT", all)
     allaccs = DB.get_key("USER_ACCS")
     del allaccs[event.sender_id][phone]
     DB.set_key("USER_ACCS", allaccs)
