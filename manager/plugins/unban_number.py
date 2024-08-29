9dc94afc51f4e5f42a1c6f55f41343c6aeb4a397
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-29 09:44:26 +0330

    update manager/plugins/unban_number.py

--- manager/plugins/unban_number.py
+++ manager/plugins/unban_number.py
@@ -33,14 +33,9 @@ def mobile():
     ]
     return random.choice(MOBILES)
 
-@Cmd(pattern="UnBan Number ♻️")
+@Cmd(pattern="\\/UnBan (.*)")
 async def unbannumber(event):
-    async with bot.conversation(event.chat_id) as conv:
-        send = await event.reply("**🔆 Ok, Send Your Phone Number To Get UnBan Text Email For This:**\n\n__• Ex: +19307777777 __", buttons=back_menu)
-        response = await conv.get_response(send.id, timeout=60)
-        phone = response.text
-    if phone in DB.get_key("CMD_LIST"):
-        return
+    phone = str(event.pattern_match.group(1))
     flag = get_flag(phone)
     osver = random.randint(20, 90)
     htext = HTML.format(phone=phone.replace("+", ""), osver=osver, device=mobile())
@@ -49,4 +44,3 @@ async def unbannumber(event):
     text = f"**💯 UnBan Text Email File For:** ( {flag} `{phone}` {flag} )\n\n**💠 Open This On Your Browser And Click To Send Email!**"
     await event.reply(text, file=hfile)
     os.remove(hfile)
-    await event.respond("**♻️ Main Menu:**", buttons=main_menu(event))
