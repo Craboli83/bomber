1142517f7cf35d4ba2533aef5a0efa638a0b14dc
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-29 10:18:58 +0330

    update manager/plugins/start.py

--- manager/plugins/start.py
+++ manager/plugins/start.py
@@ -10,6 +10,6 @@ async def start(event):
     info = await bot.get_entity(event.sender_id)
     await event.reply(f"**👋 Hi {info.first_name}!**\n**😘 Welcome To Acc Manager Robot!**\n\n**💡 Maker: @TheaBoLi**", buttons=main_menu())
 
-@Cmd(pattern="🔙")
+@Cmd(pattern="Back 🔙")
 async def back(event):
-    await event.reply("**♻️ Im Backed To Main Menu!**", buttons=main_menu(event))
+    await event.reply("**♻️ Backed To Main Menu!**", buttons=main_menu(event))
