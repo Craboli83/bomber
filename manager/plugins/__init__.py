b9fcc60a1585e1c4c589355e5816516d5a824eda
Aboli <96248859+iMaBoLi@users.noreply.github.com>
2024-08-29 10:33:46 +0330

    update manager/plugins/__init__.py

--- manager/plugins/__init__.py
+++ manager/plugins/__init__.py
@@ -7,8 +7,7 @@ def main_menu():
         [Button.text("Add Account 📥", resize=True), Button.text("Add Session 🔗", resize=True)],
         [Button.text("Accounts List 📋", resize=True)],
         [Button.text("Account Panel 🛠️", resize=True), Button.text("Account Settings ⚙️", resize=True)],
-        [Button.text("My Info 📝", resize=True)],
-        [Button.text("Guide 💡", resize=True), Button.text("Support 🧒", resize=True)],
+        [Button.text("My Info 📝", resize=True), Button.text("Guide 💡", resize=True), Button.text("Support 🧒", resize=True)],
     ]
     return menu
 
@@ -19,7 +18,6 @@ back_menu = [
 def manage_menu(phone):
     menu = [
         [Button.inline("✏️ Edit Account ✏️", data=f"editacc:{phone}")],
-        [Button.inline("💡 Get Info 💡", data=f"infoacc:{phone}")],
         [Button.inline("❗ LogOut Bot ❗", data=f"logout:{phone}"), Button.inline("🚫 Delete 🚫", data=f"delacc:{phone}")],
         [Button.inline("❌ Reset Authorizations ❌", data=f"resauths:{phone}")],
         [Button.inline("🧾 Get Authorizations 🧾", data=f"getauths:{phone}"), Button.inline("📋 Get Telegram Codes 📋", data=f"getcodes:{phone}")],
